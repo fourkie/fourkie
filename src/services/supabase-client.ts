bad45ff6a7f0ff97bb52384eccbc4e6f2876a3a3
sharet9446 <spees97@naver.com>
2025-04-04 13:41:00 +0900

    refactor: supabase 클라이언트 변수 이름을 supabaseClient로 변경

--- src/services/supabase-client.ts
+++ src/services/supabase-client.ts
@@ -2,9 +2,9 @@ import ENV from "@/constants/env";
 import { createBrowserClient } from "@supabase/ssr";
 
 // 클라이언트 전용 supabase
-const supabase = createBrowserClient(
+const supabaseClient = createBrowserClient(
   ENV.SUPABASE_URL_CLIENT!,
   ENV.SUPABASE_ANON_KEY_CLIENT!,
 );
 
-export default supabase;
+export default supabaseClient;
