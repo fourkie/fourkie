6c56a1765f70fc205bac5d36c1b9420628669144
sharet9446 <spees97@naver.com>
2025-04-04 13:27:08 +0900

    refactor: supabase 클라이언트 및 서버 함수 추가 및 리팩토링

--- src/services/supabase-client.ts
+++ src/services/supabase-client.ts
@@ -1,9 +1,10 @@
 import ENV from "@/constants/env";
 import { createBrowserClient } from "@supabase/ssr";
 
+// 클라이언트 전용 supabase
 const supabase = createBrowserClient(
-  ENV.SUPABASE_URL_CLIENT,
-  ENV.SUPABASE_ANON_KEY_CLIENT,
+  ENV.SUPABASE_URL_CLIENT!,
+  ENV.SUPABASE_ANON_KEY_CLIENT!,
 );
 
 export default supabase;
