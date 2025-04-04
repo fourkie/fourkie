6f5c3d5468b6531c8f2c9dc4574e160a4ca24b48
도니 김 <kdh4847@naver.com>
2025-04-04 19:40:11 +0900

    update: 회원가입 유효성 검사 로직 구현

--- src/app/sign-up/_components/sign-up-form.tsx
+++ src/app/sign-up/_components/sign-up-form.tsx
@@ -3,10 +3,19 @@
 import { useSignupMutation } from "@/hooks/mutations/auth-mutations";
 import { FieldValues, useForm } from "react-hook-form";
 import { FormData } from "../type";
+import {
+  EMAIL_VALIDATION,
+  NICKNAME_VALIDATION,
+  PASSWORD_VALIDATION,
+} from "@/constants/validations.constant";
 import { FORM_MESSAGE } from "@/constants/form-message";
 
 const SignUpForm = () => {
-  const { register, handleSubmit, formState } = useForm<FormData>({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
     mode: "onBlur",
   });
 
@@ -19,29 +28,26 @@ const SignUpForm = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input
-        {...register("email", {
-          required: { value: true, message: FORM_MESSAGE.EMAIL },
-        })} // pattern: {value: ..., message: ...}로 정규식 추가하여 유효성 검사
+        {...register("email", EMAIL_VALIDATION)}
         type="email"
         placeholder={FORM_MESSAGE.EMAIL}
       />
-      {formState.errors.email && <span>{formState.errors.email.message}</span>}
+      {errors.email && <span>{errors.email.message}</span>}
       <input
-        {...register("password", {
-          required: { value: true, message: FORM_MESSAGE.PASSWORD },
-        })}
+        {...register("password", PASSWORD_VALIDATION)}
         type="password"
         placeholder={FORM_MESSAGE.PASSWORD}
       />
+      {errors.password && <span>{errors.password.message}</span>}
 
       <input
-        {...register("nickname", {
-          required: { value: true, message: FORM_MESSAGE.NICKNAME },
-        })} // pattern: {value: ..., message: ...}로 정규식 추가하여 유효성 검사
+        {...register("nickname", NICKNAME_VALIDATION)}
         type="text"
         placeholder={FORM_MESSAGE.NICKNAME}
       />
-      <button disabled={!formState.isValid}>회원가입</button>
+      {errors.nickname && <span>{errors.nickname.message}</span>}
+
+      <button disabled={!isValid}>회원가입</button>
     </form>
   );
 };
--- src/constants/form-message.ts
+++ src/constants/form-message.ts
@@ -1,6 +1,6 @@
 export const FORM_MESSAGE = {
-  EMAIL: "이메일을 작성해주세요.",
-  PASSWORD: "비밀번호를 작성해주세요.",
+  EMAIL: "이메일을 입력해주세요.",
+  PASSWORD: "비밀번호를 입력해주세요.",
   PASSWORD_CONFIRM: "비밀번호를 확인해주세요.",
   NICKNAME: "닉네임을 입력해주세요.",
 };
