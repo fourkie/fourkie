13f2b17a67f8608f40c7fe20d785c3f593c34bc4
sharet9446 <spees97@naver.com>
2025-04-07 17:54:34 +0900

    refactor: 버튼 텍스트 및 결과 표시 개선

--- src/app/posting/_components/posting-form.tsx
+++ src/app/posting/_components/posting-form.tsx
@@ -37,19 +37,24 @@ const PostingForm = () => {
           type="submit"
           className="ml-2 px-4 py-2 bg-blue-500 text-white rounded"
         >
-          {isPending ? "분석 중..." : "게시"}
+          게시
         </button>
       </form>
 
       <h1 className="mt-4 font-bold">감정 분석 결과</h1>
       {emotions ? (
-        <ul className="bg-gray-100 p-4 rounded mt-2 text-sm list-disc list-inside">
-          {emotions.map((label: string, index: number) => (
-            <li key={index}>{label}</li>
-          ))}
-        </ul>
+        <>
+          <ul className="bg-gray-100 p-4 rounded mt-2 text-sm list-disc list-inside">
+            {emotions.map((label: string, index: number) => (
+              <li key={index}>{label}</li>
+            ))}
+          </ul>
+          <p> 선택된 감정에 따른 플레이 리스트를 추천해드려요</p>
+        </>
       ) : (
-        <p className="text-gray-500 mt-2">결과 없음</p>
+        <p className="text-gray-500 mt-2">
+          {isPending && "ai가 ㅇㅇ님의 기분을 분석중이에요 조금만 기다려주세오"}
+        </p>
       )}
     </div>
   );
