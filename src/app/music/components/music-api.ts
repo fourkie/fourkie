911db5f22ec348a9a47d80fcbfd1f36eb8d65ba0
Young <dndud5182@gmail.com>
2025-04-07 18:58:58 +0900

    add: Spotify API 연동 로직 정리 및 감정 기반 플레이리스트 요청 함수 구현

--- src/app/music/components/music-api.ts
+++ src/app/music/components/music-api.ts
@@ -1,56 +1,42 @@
 import { Emotion } from "@/constants/spotify";
-import { EmotionKeywords } from "@/utils/music-emotion.util";
-import { AccessToken, TrackItem } from "../type";
+import { AccessToken, PlaylistsResponse } from "../type";
 
-// Access Token 발급
+// Spotify 토큰 요청
 export const getAccessToken = async () => {
   try {
     const response = await fetch("/api/spotify/token");
-
-    if (!response.ok) {
-      console.error("AccessToken 요청 실패");
-      return null;
-    }
 
     const data: AccessToken = await response.json();
 
     return data.access_token;
   } catch (error) {
-    console.error("AccessToken 예외 발생 : ", error);
+    console.error("토큰 요청 실패 : ", error);
     return null;
   }
 };
 
-// 감정 키워드로 직접 트랙(노래) 검색
-export const getTracksByEmotion = async (
+// 감정 기반 플레이리스트 요청
+export const getEmotionPlaylists = async (
   emotion: Emotion,
   accessToken: string,
 ) => {
-  const searchKeyword = EmotionKeywords[emotion] ?? emotion.toString();
+  const query = emotion;
 
   try {
     const response = await fetch(
       `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-        searchKeyword,
-      )}&type=track&limit=30&market=KR`,
+        query,
+      )}&type=playlist&limit=30&market=KR`,
       {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
+        headers: { Authorization: `Bearer ${accessToken}` },
       },
     );
 
-    if (!response.ok) {
-      console.error("트랙 검색 실패");
-      return [];
-    }
+    const data: PlaylistsResponse = await response.json();
 
-    const data = await response.json();
-    const tracks: TrackItem[] = data.tracks.items;
-
-    return tracks;
-  } catch (err) {
-    console.error("트랙 검색 중 예외 발생 : ", err);
+    return data.playlists.items;
+  } catch (error) {
+    console.error("플레이리스트 요청 실패 : ", error);
     return [];
   }
 };
