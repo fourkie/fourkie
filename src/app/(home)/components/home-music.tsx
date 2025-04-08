aaf92eaf47b118a693e26df1c13444e684b3e0ea
usuny0317 <usuny0317@naver.com>
2025-04-08 12:29:18 +0900

    add/ 앨범 컴포넌트 추가가

---
+++ src/app/(home)/components/home-music.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomeMusic = () => {
+  return (
+    <div>
+      <div>앨범</div>
+      <div className="flex flex-col">
+        <div>제목</div>
+        <div>가수</div>
+        <div>플레이버튼</div>
+      </div>
+      HomeMusic
+    </div>
+  );
+};
+
+export default HomeMusic;
