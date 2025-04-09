6d177228bba86e2e6aaa23e8ed167a6f15b6f667
usuny0317 <usuny0317@naver.com>
2025-04-09 12:49:41 +0900

    update/ 데이터 없을 때 막기 #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import { getUserId, getUserNickname } from "@/services/home-services";
 import HomeCalendar from "./(home)/components/home-calendar";
 import HomeDate from "./(home)/components/home-date";
 import HomeHeader from "./(home)/components/home-header";
+import { redirect } from "next/navigation";
 
 const HomePage = async () => {
   const userId: string | undefined = await getUserId();
   const nickname: string | undefined = await getUserNickname();
+  if (userId || nickname === null) {
+    redirect("/sign-in");
+  }
   return (
     <div>
       <HomeHeader nickname={nickname} />
