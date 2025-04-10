d532213d40dfb45da8d84c6a78e1658d96b9758f
Young <dndud5182@gmail.com>
2025-04-10 15:47:05 +0900

    refactor: 감정 기반 플레이리스트 쿼리 함수 개선 및 불필요한 토큰 요청 제거

--- src/hooks/queries/use-get-all-playlists-by-query-query.ts
+++ src/hooks/queries/use-get-all-playlists-by-query-query.ts
@@ -2,24 +2,14 @@
 
 import { QUERY_KEY } from "@/constants/query-keys";
 import { Emotion } from "@/constants/spotify";
-import {
-  getEmotionPlaylists,
-  getSpotifyAccessToken,
-} from "@/services/music-services";
+import { fetchEmotionBasedPlaylists } from "@/services/music-services";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetAllPlaylistsByQueryQuery = (emotion: Emotion) => {
   return useQuery({
     queryKey: [QUERY_KEY.EMOTION_PLAYLISTS, emotion],
-    queryFn: async () => {
-      const accessToken = await getSpotifyAccessToken();
-
-      if (!accessToken) {
-        throw new Error("AccessToken을 가져오지 못했습니다.");
-      }
-
-      return await getEmotionPlaylists(emotion, accessToken);
-    },
-    staleTime: 1000 * 60 * 30, // 30분 동안 fresh
+    queryFn: () => fetchEmotionBasedPlaylists(emotion),
+    staleTime: 1000 * 60 * 30, // 30분 동안 캐시 유지
+    retry: 2, // 최대 2번 재시도
   });
 };
