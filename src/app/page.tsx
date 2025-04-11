6d3e6c9f68683854334e1aa586f624b451cdc85a
usuny0317 <usuny0317@naver.com>
2025-04-11 20:25:03 +0900

    move/ 홈헤더 레이아웃으로 이사하기 #3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import { getUserId, getUserNickname } from "@/services/home-service";
+import { getUserId } from "@/services/home-service";
 import HomeCalendar from "./(home)/components/home-calendar";
-import HomeHeader from "./(home)/components/home-header";
 import { redirect } from "next/navigation";
 import HomeFriend from "./(home)/components/home-friend";
 
 const HomePage = async () => {
   const userId: string | undefined = await getUserId();
-  const nickname: string | undefined = await getUserNickname();
+  //const nickname: string | undefined = await getUserNickname();
   //if (!userId || !nickname) {
   //스포티파이 구현까지 검사 로직 잠궈두기
   if (!userId) {
@@ -14,7 +13,7 @@ const HomePage = async () => {
   }
   return (
     <div>
-      <HomeHeader nickname={nickname} />
+      {/* <HomeHeader nickname={nickname} /> */}
       <HomeCalendar userId={userId} />
       <HomeFriend userId={userId} />
       <div>뮤직 컴포넌트 자리</div>
