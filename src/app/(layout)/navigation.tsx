c6756089539f8f06983d7acb690495237f6cfd9f
Young <dndud5182@gmail.com>
2025-04-11 16:25:29 +0900

    test: 음악 페이지 href 수정

--- src/app/(layout)/navigation.tsx
+++ src/app/(layout)/navigation.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 
 const Navigation = () => {
-  return <div className="flex justify-evenly items-center text-black fixed bottom-0 w-full h-[106px] pb-[34px] rounded-t-[28px] bg-white shadow-md">
-    <Link href='/'>홈</Link>
-    <Link href='/list'>리스트</Link>
-    <Link href='/music'>음악</Link>
-    <Link href='my-page'>마이페이지</Link>
-  </div>;
+  return (
+    <div className="flex justify-evenly items-center text-black fixed bottom-0 w-full h-[106px] pb-[34px] rounded-t-[28px] bg-white shadow-md">
+      <Link href="/">홈</Link>
+      <Link href="/list">리스트</Link>
+      <Link href="/music-credentials">음악</Link>
+      <Link href="my-page">마이페이지</Link>
+    </div>
+  );
 };
 
 export default Navigation;
