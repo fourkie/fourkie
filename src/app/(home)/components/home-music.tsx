30af79c847cee3be929beeaf9ddd5a4e5f0ef1ce
usuny0317 <usuny0317@naver.com>
2025-04-12 17:48:46 +0900

    style/ 홈-뮤직 부분 스타일일 #3

--- src/app/(home)/components/home-music.tsx
+++ src/app/(home)/components/home-music.tsx
@@ -1,15 +1,41 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import {
+  ChevronRight,
+  StepBack,
+  StretchVertical,
+  StepForward,
+} from "lucide-react";
+import EmotionImage from "@/ui/common/emotion-image.common";
+import EMOTION_COOKIE_IMAGE_URL from "@/constants/emotions-url.constant";
 
 const HomeMusic = () => {
+  const route = useRouter();
+  const handleHomeMusic = () => {
+    route.push("/music");
+  };
   return (
     <div>
-      <div>앨범</div>
-      <div className="flex flex-col">
-        <div>제목</div>
-        <div>가수</div>
-        <div>플레이버튼</div>
+      <div
+        className="flex justify-between items-center"
+        onClick={() => {
+          handleHomeMusic();
+        }}
+      >
+        <div className="text-xl font-bold">오늘 추천 음악</div>
+        <ChevronRight className="cursor-pointer" />
       </div>
-      HomeMusic
+      <div className="border rounded-xl bg-primary-200 flex flex-row gap-4 p-4">
+        <EmotionImage src={EMOTION_COOKIE_IMAGE_URL.SAD} size="xs" />
+        <div className="flex flex-col justify-between flex-1">
+          <div className="text-xl font-bold">땡땡할 때 듣기 좋은 음악</div>
+          <div>가수</div>
+          <div className="flex justify-between items-center">
+            <StepBack /> <StretchVertical /> <StepForward />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
