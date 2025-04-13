5f5ff2236b84c24766c5a0aceeeb7f7749af1ebd
usuny0317 <usuny0317@naver.com>
2025-04-13 13:58:01 +0900

    update/ 홈 헤더에 sign-in/up 못가게 조건부 렌더링 #3

--- src/app/(home)/components/home-header.tsx
+++ src/app/(home)/components/home-header.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 const HomeHeader = ({ nickname }: { nickname: string | undefined }) => {
+  /* const HomeHeader = () => { */
+  const pathname = usePathname();
   const route = useRouter();
+
+  if (pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up")) {
+    return null;
+  }
+
   const handlePlus = () => {
     route.push("/posting");
   };
   return (
     <div className="flex flex-row items-center justify-between bg-primary-50 p-3">
       <div className="text-2xl mx-auto">{nickname}님</div>
+      {/* <div className="text-2xl mx-auto">두두님</div> */}
       <div
         className="text-3xl font-bold cursor-pointer text-white hover:text-secondary-200"
         onClick={handlePlus}
