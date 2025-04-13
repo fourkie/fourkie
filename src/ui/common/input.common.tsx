5a847574b69fc79e4509aaaa4096a0fbd1fc1e21
sharet9446 <spees97@naver.com>
2025-04-13 18:17:37 +0900

    fix: label 렌더링 조건을 수정하고 input 클래스 이름을 추가하여 스타일 일관성 유지

--- src/ui/common/input.common.tsx
+++ src/ui/common/input.common.tsx
@@ -5,9 +5,9 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
   ({ label, error, className, ...props }, ref) => {
     return (
       <div className="flex flex-col items-center">
-        {label && <label>{label}</label>}
+        <label>{label}</label>
 
-        <input ref={ref} {...props} />
+        <input ref={ref} className={`${className}`} {...props} />
 
         {error && <span className="text-xs text-red-500">{error}</span>}
       </div>
