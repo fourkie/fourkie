065f068c1c099b104227e8d240e5785f322ef8a3
usuny0317 <usuny0317@naver.com>
2025-04-13 22:17:08 +0900

    style/ 친구 css 다듬기 #3

--- src/app/(home)/components/home-friend.tsx
+++ src/app/(home)/components/home-friend.tsx
@@ -26,19 +26,27 @@ const HomeFriend = ({ userId }: { userId: string }) => {
 
   if (isPending) {
     return (
-      <div className="border rounded-xl bg-primary-200 p-4 my-4">
-        로딩 중...
-      </div>
+      <div className="border rounded-xl bg-primary-50 p-4 my-4">로딩 중...</div>
     );
   }
 
   //친구가 없는 or 친구가 글을 안 써서 친구 포스트가 없는 경우
   if (!friendPostsForToday || friendPostsForToday.length === 0) {
     return (
-      <div className="border rounded-xl bg-primary-200 p-4 my-4">
-        <EmotionImage src={EMOTION_COOKIE_IMAGE_URL.SAD} size="xs" />
-        <div className="text-xl font-bold">친구의 쿠키가 없네요!</div>
-        <div>친구를 추가해보는 건 어떨까요?</div>
+      <div>
+        <div className="flex justify-between items-center mt-6">
+          <div className="text-xl font-bold">
+            오늘 하루 내 친구들 기분을 살펴볼까요?
+          </div>
+          <ChevronRight className="cursor-pointer" />
+        </div>
+        <div className="border rounded-xl bg-primary-50 flex flex-row items-center gap-4 p-4">
+          <EmotionImage src={EMOTION_COOKIE_IMAGE_URL.SAD} size="xs" />
+          <div className="flex flex-col">
+            <div className="text-xl font-bold">친구의 쿠키가 없네요!</div>
+            <div>친구를 추가해보는 건 어떨까요?</div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -52,7 +60,7 @@ const HomeFriend = ({ userId }: { userId: string }) => {
         <ChevronRight className="cursor-pointer" />
       </div>
 
-      <div className="border rounded-xl bg-primary-200 flex flex-row items-center gap-4 p-4">
+      <div className="border rounded-xl bg-primary-50 flex flex-row items-center gap-4 p-4">
         <EmotionImage
           src={checkEmotion(friendPostsForToday?.[0]?.post_emotion)}
           size="xs"
