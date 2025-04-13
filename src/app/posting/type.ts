f3e00c4c18ec577da7204bd5bd752f06ef669a00
sharet9446 <spees97@naver.com>
2025-04-13 16:29:43 +0900

    update: 타입 추가

--- src/app/posting/type.ts
+++ src/app/posting/type.ts
@@ -12,16 +12,19 @@ export type UserDateProps = {
 };
 
 export type PostingResultModalProps = {
-  emotions: string[];
+  emotion: string;
   isPending: boolean;
   nickname: string | undefined;
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-// test 타입입니다.
-export type Mood = {
-  name: string;
-  icon: string;
-  color: string;
+export type PostingEmotionModalSlideProps = {
+  currentEmotionKey: string;
+  emotionKeys: string[];
+  setSlideIndex: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export type PostingEmotionModalButtonProps = {
+  onClose: () => void;
 };
