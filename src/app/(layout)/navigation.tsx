97977971030a9b1441e5ac57f318dddada6b946e
Young <dndud5182@gmail.com>
2025-04-13 18:50:29 +0900

    update: Music 페이지 href 수정

--- src/app/(layout)/navigation.tsx
+++ src/app/(layout)/navigation.tsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     <div className="flex justify-evenly items-center text-black fixed bottom-0 w-full h-[106px] pb-[34px] rounded-t-[28px] bg-white shadow-md">
       <Link href="/">홈</Link>
       <Link href="/list">리스트</Link>
-      <Link href="/music-credentials">음악</Link>
+      <Link href="/music">음악</Link>
       <Link href="my-page">마이페이지</Link>
     </div>
   );
