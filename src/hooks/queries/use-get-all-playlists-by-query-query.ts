4b0a7bbaa543412bca596cebcbb9f04100e07d46
Young <dndud5182@gmail.com>
2025-04-13 23:06:46 +0900

    refactor: fetchAccessToken 및 fetchSpotifyPlaylistList 기능을 music-service.ts 파일로 분리

--- src/hooks/queries/use-get-all-playlists-by-query-query.ts
+++ src/hooks/queries/use-get-all-playlists-by-query-query.ts
@@ -1,27 +1,13 @@
-import {
-  SpotifyAccessToken,
-  SpotifyPlaylistItem,
-  SpotifyPlaylistList,
-} from "@/app/music/type";
+import { SpotifyAccessToken } from "@/app/music/type";
 import { QUERY_KEY } from "@/constants/query-keys.constant";
-import { SPOTIFY } from "@/constants/spotify.constant";
-import { TOAST_MESSAGE } from "@/constants/toast-message.constant";
+import {
+  fetchAccessToken,
+  fetchSpotifyPlaylistList,
+} from "@/services/music-service";
 import { useQuery } from "@tanstack/react-query";
 
-export const useGetAllPlaylistsByQueryQuery = (searchTerm: string) => {
+export const useGetAllPlaylistsByQueryQuery = (query: string) => {
   // ① 토큰 요청
-  const fetchAccessToken = async () => {
-    const response = await fetch(SPOTIFY.CALLBACK_ROUTE);
-
-    if (!response.ok) {
-      throw new Error(TOAST_MESSAGE.SPOTIFY.ACCESS_TOKEN_ERROR);
-    }
-
-    const data = response.json();
-
-    return data;
-  };
-
   const {
     data: tokenData,
     error: tokenError,
@@ -46,38 +32,10 @@ export const useGetAllPlaylistsByQueryQuery = (searchTerm: string) => {
     isPending: playlistsPending,
     refetch,
   } = useQuery({
-    queryKey: [QUERY_KEY.SPOTIFY_PLAYLISTS, searchTerm, accessToken],
-    queryFn: async () => {
-      if (!accessToken) {
-        throw new Error(TOAST_MESSAGE.SPOTIFY.ACCESS_TOKEN_ERROR);
-      }
-
-      const apiUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
-        searchTerm,
-      )}&type=playlist&limit=50&offset=0`;
-
-      const response = await fetch(apiUrl, {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
-        },
-      });
-
-      if (!response.ok || response.status === 401) {
-        throw new Error(TOAST_MESSAGE.SPOTIFY.ERROR);
-      }
-
-      const data = await response.json();
-
-      const filteredPlaylists: SpotifyPlaylistList =
-        data.playlists.items.filter(
-          (item: SpotifyPlaylistItem) => item !== null,
-        );
-
-      // null이 아닌 플레이리스트 항목만 필터링
-      return filteredPlaylists;
-    },
+    queryKey: [QUERY_KEY.SPOTIFY_PLAYLISTS, query, accessToken],
+    queryFn: () => fetchSpotifyPlaylistList(accessToken, query),
     // 토큰과 검색어가 있을 때만 실행
-    enabled: !!accessToken && !!searchTerm,
+    enabled: !!accessToken && !!query,
   });
 
   return {
@@ -87,6 +45,6 @@ export const useGetAllPlaylistsByQueryQuery = (searchTerm: string) => {
     playlistsError,
     tokenPending,
     playlistsPending,
-    refetch, // 플레이리스트 재요청 함수
+    refetch,
   };
 };
---
+++ src/services/music-service.ts
@@ -0,0 +1,51 @@
+import {
+  SpotifyAccessToken,
+  SpotifyPlaylistItem,
+  SpotifyPlaylistList,
+} from "@/app/music/type";
+import { SPOTIFY } from "@/constants/spotify.constant";
+import { TOAST_MESSAGE } from "@/constants/toast-message.constant";
+
+export const fetchAccessToken = async () => {
+  const response = await fetch(SPOTIFY.CALLBACK_ROUTE);
+
+  if (!response.ok) {
+    throw new Error(TOAST_MESSAGE.SPOTIFY.ACCESS_TOKEN_ERROR);
+  }
+
+  const data = await response.json();
+
+  return data;
+};
+
+export const fetchSpotifyPlaylistList = async (
+  accessToken: SpotifyAccessToken,
+  query: string,
+) => {
+  if (!accessToken) {
+    throw new Error(TOAST_MESSAGE.SPOTIFY.ACCESS_TOKEN_ERROR);
+  }
+
+  const apiUrl = `https://api.spotify.com/v1/search?q=${encodeURIComponent(
+    query,
+  )}&type=playlist&limit=50&offset=0`;
+
+  const response = await fetch(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  if (!response.ok || response.status === 401) {
+    throw new Error(TOAST_MESSAGE.SPOTIFY.ERROR);
+  }
+
+  const data = await response.json();
+
+  // null이 아닌 플레이리스트 항목만 필터링
+  const filteredPlaylists: SpotifyPlaylistList = data.playlists.items.filter(
+    (item: SpotifyPlaylistItem) => item !== null,
+  );
+
+  return filteredPlaylists;
+};
