0ec8bce267e44947b482056f0f07713137f8c53c
sharet9446 <spees97@naver.com>
2025-04-14 16:58:28 +0900

    update: createPost 및 updatePostsByPostId 함수 추가

--- src/services/post-service.ts
+++ src/services/post-service.ts
@@ -1,3 +1,7 @@
+import {
+  CreatePostParams,
+  updatePostsByPostIdParams,
+} from "@/app/posting/type";
 import createClient from "./supabase-client-service";
 
 export const getAllPosts = async () => {
@@ -17,6 +21,52 @@ export const getAllPostsById = async ({ userId }: { userId: string }) => {
       .from("posts")
       .select("*")
       .eq("user_id", userId);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const createPost = async ({
+  userId,
+  title,
+  content,
+  currentEmotion,
+}: CreatePostParams) => {
+  const supabaseClient = createClient();
+  try {
+    const { data } = await supabaseClient.from("posts").insert([
+      {
+        user_id: userId,
+        post_title: title,
+        post_content: content,
+        post_emotion: currentEmotion,
+      },
+    ]);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updatePostsByPostId = async ({
+  postId,
+  title,
+  content,
+  currentEmotion,
+}: updatePostsByPostIdParams) => {
+  const supabaseClient = createClient();
+  try {
+    const { data } = await supabaseClient
+      .from("posts")
+      .update({
+        post_title: title,
+        post_content: content,
+        post_emotion: currentEmotion,
+      })
+      .eq("post_id", postId);
+
     return data;
   } catch (error) {
     console.log(error);
