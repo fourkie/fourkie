581513241f52d163005f93898ed2c591655e8ee0
usuny0317 <usuny0317@naver.com>
2025-04-14 11:53:45 +0900

    refactor/ 커스텀 훅 삭제 쿼리만 사용 #3

--- src/app/(home)/components/home-header.tsx
+++ src/app/(home)/components/home-header.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import { useRefreshHeader } from "@/hooks/custom/use-refresh-header";
+import { useGetUserNicknameByIdQuery } from "@/hooks/queries/use-get-user-nickname-by-id-query";
 import { getUserIdClient } from "@/services/home-client-service";
+import Link from "next/link";
 
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const HomeHeader = () => {
   const pathname = usePathname();
-  const route = useRouter();
   const [userId, setUserId] = useState<string | undefined>(undefined);
-  //커스텀 훅으로 클라이언트 사이드 라이프사이클에 의존하는 것들을 안전하게 클라이언트 전용으로 사용
-  const { nickname, loading, isLoggedIn } = useRefreshHeader(userId);
 
+  //주스탄드로 리렌더링 될 때 userId 값 가져오게
   useEffect(() => {
     const fetchUser = async () => {
       const userid = await getUserIdClient();
@@ -22,27 +21,29 @@ const HomeHeader = () => {
     };
     fetchUser();
   }, []);
-
-  if (loading) return null;
-  if (!isLoggedIn) return null;
-  if (!nickname || !isLoggedIn) return <div>잘못된 접근</div>;
+  const {
+    data: nickname,
+    isLoading,
+    isError,
+  } = useGetUserNicknameByIdQuery(userId, {
+    enabled: Boolean(userId),
+    // 언디파인드일 경우 막기
+  });
 
   if (pathname.startsWith("/sign-in") || pathname.startsWith("/sign-up")) {
     return null;
   }
 
-  const handlePlus = () => {
-    route.push("/posting");
-  };
+  if (!nickname || isLoading || isError) return null;
+
   return (
     <div className="flex flex-row items-center justify-between bg-primary-50 p-3">
-      <div className="text-2xl mx-auto">{nickname}님</div>
-      <div
-        className="text-3xl font-bold cursor-pointer text-white hover:text-secondary-200"
-        onClick={handlePlus}
-      >
-        +
-      </div>
+      <div className="text-2xl mx-auto">{nickname.user_nickname}님</div>
+      <Link href={"/posting"}>
+        <div className="text-3xl font-bold cursor-pointer text-white hover:text-secondary-200">
+          +
+        </div>
+      </Link>
     </div>
   );
 };
