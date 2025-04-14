4d66c480cdeab3cb11788c2703e38ea62f4cad36
usuny0317 <usuny0317@naver.com>
2025-04-14 12:15:39 +0900

    refactor/ 주스탄드 타입 분리 #3

--- src/hooks/zustand/store.ts
+++ src/hooks/zustand/store.ts
@@ -1,11 +1,7 @@
-// store/useLoginStore.ts
+import { LoginStore } from "@/types/zustand.type";
 import { create } from "zustand";
 
-interface LoginStore {
-  loginStateKey: number;
-  updateLoginStateKey: () => void;
-}
-
+//현재 시간을 기반으로 loginStateKey를 갱신
 export const useLoginStore = create<LoginStore>((set) => ({
   loginStateKey: Date.now(),
   updateLoginStateKey: () => set({ loginStateKey: Date.now() }),
