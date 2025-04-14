69711cec70aee76250c3ab79c2f3a640f2bd0be8
sharet9446 <spees97@naver.com>
2025-04-14 17:09:03 +0900

    update: 게시글 저장 기능 추가 및 버튼 클릭 처리 개선

--- src/app/posting/_components/posting-emotion-modal-button.tsx
+++ src/app/posting/_components/posting-emotion-modal-button.tsx
@@ -1,20 +1,52 @@
+import { useRouter } from "next/navigation";
 import { PostingEmotionModalButtonProps } from "../type";
+import { toast } from "react-toastify";
+import { useCreatePostMutation } from "@/hooks/mutations/use-create-post-mutation";
 
 const PostingEmotionModalButton = ({
+  userId,
+  title,
+  content,
+  currentEmotion,
   onClose,
-}: PostingEmotionModalButtonProps) => (
-  <div className="flex gap-3 font-bold text-lg">
-    <button
-      className="flex-1 py-2 px-4 bg-primary-500 rounded-2xl text-white"
-      onClick={onClose}
-    >
-      취소
-    </button>
+}: PostingEmotionModalButtonProps) => {
+  const router = useRouter();
 
-    <button className="flex-1 py-2 px-4 bg-primary-200 rounded-2xl text-black">
-      확인
-    </button>
-  </div>
-);
+  const { mutate, isPending } = useCreatePostMutation();
 
+  // 버튼 클릭 시 게시글 저장
+  const handleSave = () => {
+    mutate(
+      { userId, title, content, currentEmotion },
+      {
+        onSuccess: () => {
+          toast.success("게시물이 성공적으로 저장되었습니다!");
+          router.push("/music");
+        },
+        onError: (error) => {
+          toast.error("게시물 저장 중 오류가 발생했습니다.");
+          console.error(error);
+        },
+      },
+    );
+  };
+
+  return (
+    <div className="flex gap-3 font-bold text-lg">
+      <button
+        className="flex-1 py-2 px-4 bg-primary-500 rounded-2xl text-white"
+        onClick={onClose}
+      >
+        취소
+      </button>
+
+      <button
+        className="flex-1 py-2 px-4 bg-primary-200 rounded-2xl text-black"
+        onClick={handleSave}
+      >
+        {isPending ? "저장 중..." : "확인"}
+      </button>
+    </div>
+  );
+};
 export default PostingEmotionModalButton;
