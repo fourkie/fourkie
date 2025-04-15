adb29db89bdd5b7531a8e325f83404ac0e06d940
sharet9446 <spees97@naver.com>
2025-04-16 01:16:47 +0900

    refactor: 게시글 날짜 및 요일 표시 방식 개선

--- src/app/posting/[id]/page.tsx
+++ src/app/posting/[id]/page.tsx
@@ -16,15 +16,21 @@ const Edit = async ({ params }: { params: { id: string } }) => {
     redirect("/sign-in");
   }
   const userId = user.id;
+  const nickname = user.user_metadata.user_nickname || user.user_metadata.name;
 
   dayjs.locale("ko");
-  const dateWithDay = dayjs().format("D dddd");
+  const date = dayjs().format("D");
+  const day = dayjs().format("dddd");
 
   return (
-    <>
-      <div>{dateWithDay}</div>
-      <PostingForm postId={postId} userId={userId} />
-    </>
+    <div className="ml-5 mr-6">
+      <div className="flex gap-2 pt-4 font-bold">
+        <span className="text-xl">{date}</span>
+        <span className="text-lg">{day}</span>
+      </div>
+      <hr className="my-2.5 w-full border-grey-0 " />
+      <PostingForm postId={postId} userId={userId} nickname={nickname} />
+    </div>
   );
 };
 
