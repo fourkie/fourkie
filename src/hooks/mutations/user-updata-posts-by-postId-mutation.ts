096e057e639a2e41a4c0cd5deaa31ca87df9149b
sharet9446 <spees97@naver.com>
2025-04-15 12:28:16 +0900

    move: useUpdatePostsByPostIdMutation 훅을 사용자 게시물 업데이트를 위한 새로운 파일로 이동

--- src/hooks/mutations/user-updata-posts-by-postId-mutation.ts
+++
@@ -1,7 +0,0 @@
-import { updatePostsByPostId } from "@/services/post-service";
-import { useMutation } from "@tanstack/react-query";
-
-export const useUpdatePostsByPostIdMutation = () =>
-  useMutation({
-    mutationFn: updatePostsByPostId,
-  });
