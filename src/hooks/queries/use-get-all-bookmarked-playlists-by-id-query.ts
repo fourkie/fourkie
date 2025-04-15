96d5df3cf4529359f38d37a57991c2e1e38fc80f
Young <dndud5182@gmail.com>
2025-04-15 17:18:08 +0900

    add: 북마크 조회 useQuery 훅 작성 및 캐싱 설정 적용

---
+++ src/hooks/queries/use-get-all-bookmarked-playlists-by-id-query.ts
@@ -0,0 +1,15 @@
+import { CACHE_STALE_TIME } from "@/constants/music-constant";
+import { QUERY_KEY } from "@/constants/query-keys.constant";
+import { fetchBookmarkedPlaylists } from "@/services/music-bookmark-service";
+import { useQuery } from "@tanstack/react-query";
+
+export const useGetAllBookmarkedPlaylistsByIdQuery = (userId: string) => {
+  return useQuery({
+    queryKey: [QUERY_KEY.BOOKMARKED_PLAYLISTS, userId],
+    queryFn: () => fetchBookmarkedPlaylists(userId),
+    enabled: !!userId, // userId가 없으면 쿼리 실행 안함
+    refetchOnWindowFocus: false,
+    retry: false,
+    staleTime: CACHE_STALE_TIME, // 10분
+  });
+};
