0c3be78ab32b508360569d6a9876562b43e6b648
Young <dndud5182@gmail.com>
2025-04-15 15:27:47 +0900

    refactor: 캐시 사용 안 함 및 accessToken 처리 로직 개선

--- src/services/music-service.ts
+++ src/services/music-service.ts
@@ -52,9 +52,9 @@ export const fetchSpotifyPlaylistList = async (
 
     // 토큰 만료 → 새 토큰 발급 요청
     if (response.status === 401) {
-      const newAccessToken = await fetchAccessToken();
+      const data = await fetchAccessToken();
 
-      return await fetchWithToken(newAccessToken.accessToken);
+      return await fetchWithToken(data.accessToken);
     }
 
     // 그 외 실패 처리
