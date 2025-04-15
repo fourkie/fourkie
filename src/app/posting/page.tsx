215dea54f45d897aa3b56ff5bcffbf86cbc687ae
sharet9446 <spees97@naver.com>
2025-04-15 17:01:11 +0900

    update: 게시글 페이지에 현재 날짜 표시 기능 추가

--- src/app/posting/page.tsx
+++ src/app/posting/page.tsx
@@ -1,6 +1,8 @@
 import createClient from "@/services/supabase-server-service";
 import PostingForm from "./_components/posting-form";
 import { redirect } from "next/navigation";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
 
 const Posting = async () => {
   const supabaseServer = createClient();
@@ -13,7 +15,16 @@ const Posting = async () => {
     data.user.user_metadata.user_nickname || data.user.user_metadata.name;
   const userId = data.user.id;
 
-  return <PostingForm userId={userId} nickname={nickname} />;
+  dayjs.locale("ko");
+
+  const dateWithDay = dayjs().format("D dddd");
+
+  return (
+    <>
+      <div>{dateWithDay}</div>
+      <PostingForm userId={userId} nickname={nickname} />
+    </>
+  );
 };
 
 export default Posting;
