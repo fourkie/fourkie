520245595dcc24a5e8ec55c6e7c8f51d7ae8a33e
usuny0317 <usuny0317@naver.com>
2025-04-15 14:20:17 +0900

    update/ text 공통 컴포넌트 생성 #3

--- src/ui/common/text.common.tsx
+++ src/ui/common/text.common.tsx
@@ -1,17 +1,39 @@
-type CommonTextProps = {
-  children: React.ReactNode;
-  type: "nickname" | "title" | "body" | "xs";
-  className?: string;
-};
-const CommonText = ({ children, type, className = "" }: CommonTextProps) => {
-  const textSizeClasss = {
-    nickname: "text-2xl mx-auto",
-    title: "text-xl font-bold ",
-    body: "text-xl text-grey-7",
+import { CommonTextProps } from "@/types/text-component.type";
+
+const CommonText = ({
+  children,
+  size,
+  type,
+  color = "black",
+  className = "",
+}: CommonTextProps) => {
+  const textSize = {
     xs: "text-xs",
+    sm: "text-sm",
+    bs: "text-base",
+    lg: "text-lg",
+    xl: "text-xl",
   };
+
+  //폰트 추가 예정정
+  const textType = {
+    h: "font-bold",
+    title: "font-bold ",
+    body: "font-bold",
+  };
+
+  const textColor = {
+    black: "text-black",
+    grey: "text-grey-6",
+    white: "text-white",
+  };
+
   return (
-    <div className={`${textSizeClasss[type]}+ ${className}`}>{children}</div>
+    <div
+      className={`${textSize[size]}+ ${textType[type]}+${textColor[color]}+ ${className}`}
+    >
+      {children}
+    </div>
   );
 };
 
