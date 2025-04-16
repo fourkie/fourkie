83d35aa335ef9c8444d695efe96ef1a4c4a36765
Young <dndud5182@gmail.com>
2025-04-16 19:58:59 +0900

    refactor: useAddBookmarkMutation 및 useRemoveBookmarkMutation 개선

--- src/hooks/mutations/use-music-bookmarks-mutation.ts
+++ src/hooks/mutations/use-music-bookmarks-mutation.ts
@@ -7,67 +7,45 @@ import {
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 
-// 북마크 추가 API를 호출하는 뮤테이션 훅
-export const useAddBookmarkMutation = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
-    mutationFn: ({
-      music_playlist_id,
-      name,
-      external_urls,
-      images,
-      tracks,
-      uri,
-      userId,
-    }: {
-      music_playlist_id: string;
-      name: string;
-      external_urls: { spotify: string };
-      images: { url: string }[];
-      tracks: { href: string; total: number };
-      uri: string;
-      userId: string;
-    }) =>
-      addBookmarkedPlaylists({
-        id: music_playlist_id,
-        name,
-        external_urls,
-        images,
-        tracks,
-        uri,
-        userId,
-      }),
-
-    onSuccess: () => {
-      toast.success(TOAST_MESSAGE.SPOTIFY.ADD_BOOKMARK_SUCCESS);
-
-      queryClient.invalidateQueries({
-        queryKey: [QUERY_KEY.BOOKMARKED_PLAYLISTS],
-      });
-    },
-
-    onError: () => {
-      toast.error(TOAST_MESSAGE.SPOTIFY.ADD_BOOKMARK_ERROR);
-    },
-  });
-};
-
-// 북마크 삭제 API를 호출하는 뮤테이션 훅
-type RemoveBookmarkInput = {
+type BookmarkedProps = {
   musicPlaylistId: string;
   userId: string;
 };
 
-export const useRemoveBookmarkMutation = () => {
+// 북마크 추가 뮤테이션 훅
+// 같은 music_playlist_id를 추가할 경우, 다른 사용자 껏도 추가됨
+export const useAddBookmarkMutation = (userId: string) => {
   const queryClient = useQueryClient();
 
-  return useMutation<void, Error, RemoveBookmarkInput>({
-    mutationFn: ({ musicPlaylistId, userId }) =>
-      removeBookmarkedPlaylists(musicPlaylistId, userId),
+  return useMutation({
+    mutationFn: addBookmarkedPlaylists,
 
     onSuccess: () => {
-      toast.success(TOAST_MESSAGE.SPOTIFY.REMOVE_BOOKMARK_SUCCESS);
+      toast.success(TOAST_MESSAGE.MUSIC.ADD_BOOKMARK_SUCCESS);
+
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEY.BOOKMARKED_PLAYLISTS, userId],
+      });
+    },
+
+    onError: () => {
+      toast.error(TOAST_MESSAGE.MUSIC.ADD_BOOKMARK_ERROR);
+    },
+  });
+};
+
+// 북마크 삭제 뮤테이션 훅
+export const useRemoveBookmarkMutation = ({
+  musicPlaylistId,
+  userId,
+}: BookmarkedProps) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => removeBookmarkedPlaylists(musicPlaylistId, userId),
+
+    onSuccess: () => {
+      toast.success(TOAST_MESSAGE.MUSIC.REMOVE_BOOKMARK_SUCCESS);
 
       queryClient.invalidateQueries({
         queryKey: [QUERY_KEY.BOOKMARKED_PLAYLISTS],
@@ -75,7 +53,7 @@ export const useRemoveBookmarkMutation = () => {
     },
 
     onError: () => {
-      toast.error(TOAST_MESSAGE.SPOTIFY.REMOVE_BOOKMARK_ERROR);
+      toast.error(TOAST_MESSAGE.MUSIC.REMOVE_BOOKMARK_ERROR);
     },
   });
 };
