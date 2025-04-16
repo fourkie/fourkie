c8f1980368bdfa04e84d58bd2fc15dd3ef874714
Young <dndud5182@gmail.com>
2025-04-16 17:18:26 +0900

    refactor: PlaylistCard 컴포넌트에서 사용자 ID 관련 코드 제거 및 북마크 토글 로직 정리

--- src/app/music/_components/playlist-card.tsx
+++ src/app/music/_components/playlist-card.tsx
@@ -1,28 +1,66 @@
+import { useState } from "react";
 import { SpotifyPlaylistItem } from "../type";
+import Image from "next/image";
+import { Star } from "lucide-react";
+import Link from "next/link";
 
 interface PlaylistCardProps {
   playlist: SpotifyPlaylistItem;
-  userId: string;
-
-  // isBookmarked: boolean;
-  // onBookmarkToggle: () => void; // 부모로부터 전달받은 함수
+  // onBookmarkToggle: (playlistId: string) => void; // 북마크 추가 / 삭제함수
 }
 
-export const PlaylistCard = ({
-  playlist,
-  userId,
-}: // isBookmarked,
-// onBookmarkToggle,
-PlaylistCardProps) => {
+export const PlaylistCard = ({ playlist }: PlaylistCardProps) => {
+  const [isBookmarkedState, setIsBookmarkedState] = useState(false);
+
+  const handleBookmarkToggle = () => {
+    // onBookmarkToggle(playlist.id);
+    console.log("setIsBookmarkedState", !isBookmarkedState);
+    setIsBookmarkedState(!isBookmarkedState); // 버튼 클릭 후 상태 변경
+
+    if (isBookmarkedState == false) {
+      // 북마크 추가
+      console.log("북마크 추가");
+      // addBookmark(playlist);
+    } else if (isBookmarkedState == true) {
+      // 북마크 삭제
+      console.log("북마크 삭제");
+    }
+  };
+
   // const { mutate: addBookmark } = useAddBookmarkMutation();
   // const { mutate: removeBookmark } = useRemoveBookmarkMutation();
-  console.log(userId);
+
   return (
-    <div>
+    <div className="flex items-center justify-start">
+      <button className="bookmark-button" onClick={handleBookmarkToggle}>
+        <Star
+          className={
+            isBookmarkedState
+              ? "fill-yellow-500 text-yellow-500"
+              : "text-yellow-500"
+          }
+          size={24}
+        />
+      </button>
+
+      <Image
+        src={playlist.images[0]?.url || "/default-image.jpg"}
+        alt={playlist.name}
+        width={50}
+        height={50}
+      />
+
       <h3>{playlist.name}</h3>
-      {/* <button onClick={onBookmarkToggle}>
-        {isBookmarked ? "북마크 삭제" : "북마크 추가"}
-      </button> */}
+
+      <p>{playlist.tracks.total}곡</p>
+
+      <Link
+        href={playlist.external_urls.spotify}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Spotify
+      </Link>
     </div>
   );
 };
