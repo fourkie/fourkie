947c07d9167187d0bc6fc435eb13e84c3597fd40
Young <dndud5182@gmail.com>
2025-04-16 19:03:15 +0900

    refactor: CACHE_STALE_TIME 상수 제거

--- src/hooks/queries/use-get-all-bookmarked-playlists-by-id-query.ts
+++ src/hooks/queries/use-get-all-bookmarked-playlists-by-id-query.ts
@@ -1,5 +1,4 @@
 import { QUERY_KEY } from "@/constants/query-keys.constant";
-import { CACHE_STALE_TIME } from "@/constants/spotify.constant";
 import { fetchBookmarkedPlaylists } from "@/services/music-bookmark-service";
 import { useQuery } from "@tanstack/react-query";
 
@@ -13,6 +12,5 @@ export const useGetAllBookmarkedPlaylistsByIdQuery = (
     enabled: !!userId, // userId가 없으면 쿼리 실행 안함
     refetchOnWindowFocus: false, // 윈도우 포커스 시 자동 리패치 방지
     retry: false, // 실패 시 재시도 방지
-    staleTime: CACHE_STALE_TIME, // 캐시가 오래된 데이터는 10분 후 갱신
   });
 };
