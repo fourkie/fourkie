7b2727eb090dbf9b79d0ad819ac4ff68ea780fc9
Young <dndud5182@gmail.com>
2025-04-16 11:10:13 +0900

    add: Spotify 사용자 오류 메시지 추가

--- src/constants/toast-message.constant.ts
+++ src/constants/toast-message.constant.ts
@@ -3,6 +3,7 @@ export const TOAST_MESSAGE = {
   HOMEERROR: "불러오는 중 오류가 났습니다.",
   SPOTIFY: {
     ERROR: "예상치 못한 오류가 발생했습니다. 잠시 후 다시 시도해주세요.",
+    USER_ERROR: "로그인이 필요합니다. 로그인 후 이용해주세요!",
     CLIENT_ERROR: "Spotify 자격 증명이 누락되었습니다.",
     ACCESS_TOKEN_ERROR: "액세스 토큰을 가져오는 데 실패했습니다.",
     PLAYLISTS_ERROR: "플레이리스트를 불러오는 데 실패했습니다.",
