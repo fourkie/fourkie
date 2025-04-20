4aa5fda993bdb514655ba525f53424ee6678cbb7
usuny0317 <usuny0317@naver.com>
2025-04-20 16:47:35 +0900

    update/ 홈-음악 고정된 노래 추천 #107

--- src/app/(home)/components/home-music.tsx
+++ src/app/(home)/components/home-music.tsx
@@ -1,9 +1,14 @@
-import EMOTION_COOKIE_IMAGE_URL from "@/constants/emotions-url.constant";
+import { EMOTIONS_QUERY } from "@/constants/emotion.constant";
+import { EMOTIONSMUSIC } from "@/constants/emotions-music.constant";
 import EmotionImage from "@/ui/common/emotion-image.common";
+import { checkEmotion } from "@/utils/home-emotion.util";
 import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 const HomeMusic = () => {
+  //데이터 받아오기기
+  const data = "SAD";
+
   return (
     <div className="my-4 flex flex-col gap-4">
       <Link href={"/music"}>
@@ -13,15 +18,17 @@ const HomeMusic = () => {
         </div>
       </Link>
       <div className="flex flex-row items-center gap-4 rounded-xl border bg-primary-50 p-4">
-        <EmotionImage src={EMOTION_COOKIE_IMAGE_URL.JOY} size="xs" />
+        <EmotionImage src={checkEmotion(data)} size="xs" />
         <div className="flex flex-1 flex-col justify-between">
           <a
-            href="https://open.spotify.com/playlist/5oKWX70uCr0CfN3GpMbMfz"
+            href={`${EMOTIONSMUSIC[data]}`}
             target="_blank"
             rel="noopener noreferrer"
             className="block rounded-xl bg-primary-50 p-4"
           >
-            <div className="font-bold">잔잔할 때 들을 노래</div>
+            <div className="font-bold">
+              {EMOTIONS_QUERY[data]}날 추천하는 노래
+            </div>
           </a>
         </div>
       </div>
---
+++ src/constants/emotions-music.constant.ts
@@ -0,0 +1,12 @@
+export const EMOTIONSMUSIC: Record<string, string> = {
+  JOY: "https://open.spotify.com/playlist/55cVwqa0o5taJ035oSFGvX",
+  EXCITED: "https://open.spotify.com/playlist/2uKrG0TvBIXtXiPFv7v1bV",
+  BUTTERFLY: "https://open.spotify.com/playlist/1N3MlJJQogJbopR11PZN6y",
+  GRATEFUL: "https://open.spotify.com/playlist/6OuStud0YPRunPY9aO6R7u",
+  CALM: "https://open.spotify.com/playlist/2Q21tYhtUgZdlv5lX6JqmR",
+  LONELY: "https://open.spotify.com/playlist/5tddxaUxMqNN1jJrbgbSqX",
+  ANXIOUS: "https://open.spotify.com/playlist/1RqpbCCaveakiqFjUoE2ED",
+  TIRED: "https://open.spotify.com/playlist/1Y5gR9pbMLdH6mXgQBTIw0",
+  SAD: "https://open.spotify.com/playlist/5PQ6wSn5IUi2Md9IuXTIj1",
+  ANGRY: "https://open.spotify.com/playlist/0a056EfCYfAarxBsFDxEnM",
+};
