bc4bb3da29d05d2e962b986f31a192297f810ef5
usuny0317 <usuny0317@naver.com>
2025-04-20 14:40:15 +0900

    fix/ 홈 뮤직 외부 링크로 변경 #107

--- src/app/(home)/components/home-music.tsx
+++ src/app/(home)/components/home-music.tsx
@@ -1,25 +1,28 @@
-import { ChevronRight } from "lucide-react";
-import EmotionImage from "@/ui/common/emotion-image.common";
 import EMOTION_COOKIE_IMAGE_URL from "@/constants/emotions-url.constant";
+import EmotionImage from "@/ui/common/emotion-image.common";
+import { ChevronRight } from "lucide-react";
 import Link from "next/link";
 
 const HomeMusic = () => {
   return (
-    <div className="flex flex-col my-4 gap-4">
+    <div className="my-4 flex flex-col gap-4">
       <Link href={"/music"}>
-        <div className="flex justify-between items-center mt-2">
-          <div className=" font-bold">오늘 추천 음악</div>
+        <div className="mt-2 flex items-center justify-between">
+          <div className="font-bold">오늘 추천 음악</div>
           <ChevronRight className="cursor-pointer" />
         </div>
       </Link>
-      <div className="border rounded-xl bg-primary-50 flex flex-row items-center gap-4 p-4">
+      <div className="flex flex-row items-center gap-4 rounded-xl border bg-primary-50 p-4">
         <EmotionImage src={EMOTION_COOKIE_IMAGE_URL.JOY} size="xs" />
-        <div className="flex flex-col justify-between flex-1">
-          <Link
-            href={"https://open.spotify.com/playlist/5oKWX70uCr0CfN3GpMbMfz"}
+        <div className="flex flex-1 flex-col justify-between">
+          <a
+            href="https://open.spotify.com/playlist/5oKWX70uCr0CfN3GpMbMfz"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block rounded-xl bg-primary-50 p-4"
           >
-            <div className=" font-bold">잔잔할 때 들을 노래</div>
-          </Link>
+            <div className="font-bold">잔잔할 때 들을 노래</div>
+          </a>
         </div>
       </div>
     </div>
