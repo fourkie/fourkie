b1648f23ddad2f335604472bdeb713934d0f4fa2
sharet9446 <spees97@naver.com>
2025-04-21 15:59:33 +0900

    update: 튜토리얼을 안봤으면 튜토리얼 페이지로 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { getUserId } from "@/services/home-service";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import HomeCalendar from "./(home)/components/home-calendar";
 import HomeFriend from "./(home)/components/home-friend";
 import HomeMusic from "./(home)/components/home-music";
 
 const HomePage = async () => {
+  const cookieStore = cookies();
+  const hasSeenTutorial = cookieStore.get("hasSeenTutorial")?.value;
+
+  if (!hasSeenTutorial || hasSeenTutorial !== "true") {
+    redirect("/tutorial");
+  }
+
   const userId: string | undefined = await getUserId();
 
   if (!userId) {
