514786d1a7f80598fffbf8901fd2d327eee07e44
usuny0317 <usuny0317@naver.com>
2025-04-21 11:50:11 +0900

    update/ 홈-뮤직 수행하기 #107

--- src/app/(home)/components/home-music.tsx
+++ src/app/(home)/components/home-music.tsx
@@ -1,65 +1,73 @@
 "use client";
 import { EMOTIONS_QUERY } from "@/constants/emotion.constant";
-import { EMOTIONSMUSIC } from "@/constants/emotions-music.constant";
-import EMOTION_COOKIE_IMAGE_URL from "@/constants/emotions-url.constant";
+import { useGetAllPlaylistsByQueryQuery } from "@/hooks/queries/use-get-all-playlists-by-query-query";
 import { useGetPostTodayEmotionByIdQuery } from "@/hooks/queries/use-get-posts-today-emotion-by-id-query";
-import EmotionImage from "@/ui/common/emotion-image.common";
-import { checkEmotion } from "@/utils/home-emotion.util";
 import { ChevronRight } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 const HomeMusic = ({ userId }: { userId: string }) => {
-  const { data, isPending, isError } = useGetPostTodayEmotionByIdQuery(userId);
+  const { data } = useGetPostTodayEmotionByIdQuery(userId);
 
-  if (isPending || data === undefined || isError || data.length == 0) {
+  const emotion: string = data?.[0]?.post_emotion ?? "JOY";
+
+  const { playlists, playlistsPending } =
+    useGetAllPlaylistsByQueryQuery(emotion);
+
+  if (playlistsPending) {
     return (
-      <div className="my-4 flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <Link href={"/music"}>
           <div className="mt-2 flex items-center justify-between">
             <div className="font-bold">오늘 추천 음악</div>
             <ChevronRight className="cursor-pointer" />
           </div>
         </Link>
-        <div className="flex flex-row items-center gap-4 rounded-xl border bg-primary-50 p-4">
-          <EmotionImage src={EMOTION_COOKIE_IMAGE_URL["JOY"]} size="xs" />
-          <div className="flex flex-1 flex-col justify-between">
-            <a
-              href={`${EMOTIONSMUSIC["JOY"]}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block rounded-xl bg-primary-50 p-4"
-            >
-              <div className="font-bold">
-                {EMOTIONS_QUERY["JOY"]}날 추천하는 노래
-              </div>
-            </a>
-          </div>
+        <div className="gap-4 rounded-xl border bg-primary-50 p-4">
+          플레이리스트를 불러오고 있습니다!
         </div>
       </div>
     );
   }
+  const playNum = playlists.length;
+  const randomIndex = playNum > 0 ? Math.floor(Math.random() * playNum) : 1;
 
   return (
-    <div className="my-4 flex flex-col gap-4">
+    <div className="flex flex-col gap-4">
       <Link href={"/music"}>
         <div className="mt-2 flex items-center justify-between">
-          <div className="font-bold">오늘 추천 음악</div>
+          <div className="font-bold">
+            {EMOTIONS_QUERY[emotion]} 날 추천하는 노래
+          </div>
           <ChevronRight className="cursor-pointer" />
         </div>
       </Link>
       <div className="flex flex-row items-center gap-4 rounded-xl border bg-primary-50 p-4">
-        <EmotionImage src={checkEmotion(data[0]?.post_emotion)} size="xs" />
-        <div className="flex flex-1 flex-col justify-between">
-          <a
-            href={`${EMOTIONSMUSIC[data[0]?.post_emotion]}`}
+        <div className="justify-arround flex h-[52px] items-center gap-3">
+          <Link
+            href={playlists[randomIndex].external_urls.spotify}
             target="_blank"
             rel="noopener noreferrer"
-            className="block rounded-xl bg-primary-50 p-4"
+            className="flex w-full items-center justify-between gap-4 whitespace-nowrap"
           >
-            <div className="font-bold">
-              {EMOTIONS_QUERY[data[0]?.post_emotion]}날 추천하는 노래
+            <div className="flex h-[50px] w-12 items-center justify-center overflow-hidden">
+              <Image
+                src={
+                  playlists[randomIndex].images[0]?.url || "/default-image.jpg"
+                }
+                alt={playlists[randomIndex].name}
+                width={52}
+                height={52}
+                className="object-fit rounded-l"
+              />
             </div>
-          </a>
+            <p className="line-clamp-1 w-52 text-ellipsis whitespace-nowrap font-bold">
+              {playlists[randomIndex].name}
+            </p>
+            <p className="break-all text-sm text-grey-3">
+              {playlists[randomIndex].tracks.total}곡
+            </p>
+          </Link>
         </div>
       </div>
     </div>
