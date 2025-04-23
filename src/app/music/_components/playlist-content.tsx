d1b42636c0383006f06e58bd63525e2ecd0c9b3c
Young <dndud5182@gmail.com>
2025-04-23 15:32:30 +0900

    add: 추천 플리 및 북마크 컴포넌트 통합

---
+++ src/app/music/_components/playlist-content.tsx
@@ -0,0 +1,65 @@
+import { Emotion } from "@/constants/spotify.constant";
+import { TOAST_MESSAGE } from "@/constants/toast-message.constant";
+import { useGetAllBookmarkedPlaylistsByIdQuery } from "@/hooks/queries/use-get-all-bookmarked-playlists-by-id-query";
+import { useGetAllPlaylistsByQueryQuery } from "@/hooks/queries/use-get-all-playlists-by-query-query";
+import { PlaylistContentProps, PlaylistTabProps } from "../type";
+import PlaylistCard from "./playlist-card";
+
+const PlaylistContent = ({
+  userId,
+  activeTab,
+  emotion,
+}: PlaylistContentProps) => {
+  // 추천 플리
+  const {
+    playlists: playlistsData,
+    isPending: playlistsIsPending,
+    error: playlistsError,
+  } = useGetAllPlaylistsByQueryQuery(emotion ? Emotion[emotion] : "JOY");
+
+  // 즐겨찾기
+  const {
+    data: bookmarkedData,
+    isPending: bookmarkedIsPending,
+    error: bookmarkedError,
+  } = useGetAllBookmarkedPlaylistsByIdQuery(userId);
+
+  // 추천 플리 탭
+  if (activeTab === PlaylistTabProps.RECOMMEND) {
+    if (playlistsIsPending)
+      return <p>{TOAST_MESSAGE.MUSIC.PLAYLISTS_PENDING}</p>;
+    if (playlistsError) return <p>{TOAST_MESSAGE.MUSIC.PLAYLISTS_ERROR}</p>;
+
+    return (
+      <ul className="flex flex-col gap-3">
+        {playlistsData.map((playlist) => (
+          <PlaylistCard key={playlist.id} userId={userId} playlist={playlist} />
+        ))}
+      </ul>
+    );
+  }
+
+  // 즐겨찾기 탭
+  if (activeTab === PlaylistTabProps.BOOKMARK) {
+    if (bookmarkedIsPending)
+      return <p>{TOAST_MESSAGE.MUSIC.BOOKMARK_PENDING}</p>;
+
+    if (bookmarkedError) return <p>{TOAST_MESSAGE.MUSIC.BOOKMARK_ERROR}</p>;
+
+    if (bookmarkedData && bookmarkedData.length === 0)
+      return <p>{TOAST_MESSAGE.MUSIC.EMPTY_BOOKMARK}</p>;
+
+    return (
+      <ul className="flex flex-col gap-3">
+        {bookmarkedData?.map((playlist, index) => {
+          const key = playlist.id ?? `${playlist.name}-${index}`;
+          return <PlaylistCard key={key} playlist={playlist} userId={userId} />;
+        })}
+      </ul>
+    );
+  }
+
+  return null;
+};
+
+export default PlaylistContent;
