2cf23add8673db7724b7ca521232332004f63bcd
Young <dndud5182@gmail.com>
2025-04-23 18:35:28 +0900

    add: 감정 선택 컴포넌트에 드롭다운 버튼 추가 및 색상 관리 개선

--- src/app/music/_components/emotion-select.tsx
+++ src/app/music/_components/emotion-select.tsx
@@ -1,5 +1,11 @@
+import {
+  EMOTION_BORDER_COLOR,
+  EMOTION_COLOR,
+} from "@/constants/emotion-color.constant";
 import { EMOTIONS_QUERY } from "@/constants/emotion.constant";
 import { Emotion } from "@/constants/spotify.constant";
+import { ChevronDown, ChevronUp } from "lucide-react";
+import { useState } from "react";
 import { EmotionSelectProps } from "../type";
 
 const EmotionSelect = ({
@@ -7,24 +13,54 @@ const EmotionSelect = ({
   onChange,
   todayEmotion,
 }: EmotionSelectProps) => {
+  const [open, setOpen] = useState(false);
+
+  const handleSelect = (emotion: EmotionSelectProps["emotion"]) => {
+    onChange(emotion);
+    setOpen(false);
+  };
+
   return (
-    <div className="left-2/1 absolute top-16 pl-6">
+    <div className="left-2/1 absolute top-10 pl-6">
       <div className="mb-2 flex items-center gap-3">
-        <select
-          value={emotion}
-          onChange={(e) => onChange(e.target.value as keyof typeof Emotion)}
-          className="w-26 h-7 rounded-full border-b border-b-primary-300 bg-primary-100 px-2 py-1 text-center text-sm font-bold text-primary-700"
+        {/* 드롭다운 버튼 */}
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="flex h-[30px] max-w-72 items-center justify-center gap-1 rounded-full bg-primary-100 px-2 py-1 text-center text-sm font-bold text-primary-700"
+          style={{
+            backgroundColor: `var(--color-${EMOTION_COLOR[emotion]})`,
+            // borderColor: `var(--color-${EMOTION_BORDER_COLOR[emotion]})`,
+          }}
         >
-          {Object.entries(EMOTIONS_QUERY).map(([emotionKey]) => {
-            return (
-              <option key={emotionKey} value={emotionKey}>
-                {EMOTIONS_QUERY[emotionKey]} 날
-              </option>
-            );
-          })}
-        </select>
+          {EMOTIONS_QUERY[emotion]} 날
+          {open ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
+        </button>
         <p className="text-xl font-bold text-white">듣기 좋은</p>
       </div>
+
+      {/* 드롭다운 목록 */}
+      {open && (
+        <div className="fixed grid max-w-72 grid-cols-3 gap-4 rounded-xl bg-primary-100 p-3">
+          {Object.entries(EMOTIONS_QUERY).map(([emotionKey, emotionLabel]) => {
+            if (emotion === emotionKey) return null;
+            const isSelected = emotion === emotionKey;
+
+            return (
+              <button
+                key={emotionKey}
+                onClick={() => handleSelect(emotionKey as keyof typeof Emotion)}
+                className={`w-full rounded-full border px-2 py-1 text-sm font-bold ${isSelected ? "text-primary-900 bg-primary-300" : "bg-white text-black hover:bg-gray-100"}`}
+                style={{
+                  backgroundColor: `var(--color-${EMOTION_COLOR[emotionKey]})`,
+                  borderColor: `var(--color-${EMOTION_BORDER_COLOR[emotionKey]})`,
+                }}
+              >
+                {emotionLabel}
+              </button>
+            );
+          })}
+        </div>
+      )}
       <p className="text-xl font-bold text-white">
         플레이리스트를 추천해 드릴게요!
       </p>
---
+++ src/constants/emotion-color.constant.ts
@@ -0,0 +1,25 @@
+export const EMOTION_COLOR: Record<string, string> = {
+  JOY: "primary-400",
+  EXCITED: "primary-300",
+  BUTTERFLY: "primary-200",
+  GRATEFUL: "primary-100",
+  CALM: "primary-50",
+  LONELY: "secondary-50",
+  ANXIOUS: "secondary-100",
+  TIRED: "secondary-200",
+  SAD: "secondary-300",
+  ANGRY: "secondary-400",
+};
+
+export const EMOTION_BORDER_COLOR: Record<string, string> = {
+  JOY: "primary-300",
+  EXCITED: "primary-300",
+  BUTTERFLY: "primary-300",
+  GRATEFUL: "primary-300",
+  CALM: "primary-300",
+  LONELY: "secondary-100",
+  ANXIOUS: "secondary-300",
+  TIRED: "secondary-400",
+  SAD: "secondary-500",
+  ANGRY: "secondary-700",
+};
