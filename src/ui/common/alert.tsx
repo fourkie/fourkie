f28753485f97a09a4d3ff0af03824124e76c5902
Juha Yoon <ijooha16@gmail.com>
2025-04-23 15:12:57 +0900

    style: 색상 변경

--- src/ui/common/alert.tsx
+++ src/ui/common/alert.tsx
@@ -11,8 +11,8 @@ const Alert = ({ children, title, contents }: AlertProps) => {
     <Popup>
       <div className="w-full rounded-2xl bg-white p-4">
         <div className="flex flex-col items-center justify-center p-4">
-          <div className="text-lg font-bold">{title}</div>
-          <div className="mt-2 text-sm text-gray-500">{contents}</div>
+          <div className="text-lg text-grey-7 font-bold">{title}</div>
+          <div className="mt-2 text-sm text-grey-5">{contents}</div>
         </div>
         {children}
       </div>
