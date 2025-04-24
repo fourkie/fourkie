8569f98b596fa4ff1a718a4cabc20996740d33ca
Young <dndud5182@gmail.com>
2025-04-24 15:17:40 +0900

    refactor: 감정 선택 컴포넌트의 드롭다운 버튼 및 목록 스타일 개선

--- src/app/music/_components/emotion-select.tsx
+++ src/app/music/_components/emotion-select.tsx
@@ -20,56 +20,63 @@ const EmotionSelect = ({
     setOpen(false);
   };
 
+  const whiteText = emotion === "SAD" || emotion === "ANGRY";
+  const emotionKeys = Object.keys(EMOTIONS_QUERY) as (keyof typeof Emotion)[];
+
   return (
-    <div className="left-2/1 absolute top-10 pl-6">
-      <div className="mb-2 flex items-center gap-3">
+    <div className="absolute left-5 top-14">
+      <div className="mb-3 flex items-center gap-2">
         {/* 드롭다운 버튼 */}
         <button
           onClick={() => setOpen((prev) => !prev)}
-          className="flex h-[30px] max-w-72 items-center justify-center gap-1 rounded-full bg-primary-100 px-2 py-1 text-center text-sm font-bold text-primary-700"
+          className={`flex h-[30px] max-w-72 items-center justify-center gap-1 px-2 py-1 text-center text-sm font-bold ${
+            whiteText ? "text-white" : "text-primary-700"
+          } ${open ? "rounded-tl-[12px] rounded-tr-[12px]" : "rounded-xl"}`}
           style={{
             backgroundColor: `var(--color-${EMOTION_COLOR[emotion]})`,
-            // borderColor: `var(--color-${EMOTION_BORDER_COLOR[emotion]})`,
           }}
         >
           {EMOTIONS_QUERY[emotion]} 날
           {open ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
         </button>
-        <p className="text-xl font-bold text-white">듣기 좋은</p>
+
+        {/* 드롭다운 목록 */}
+        {open && (
+          <div className="absolute top-[30px] grid max-w-72 grid-cols-3 gap-4 rounded-bl-[12px] rounded-br-[12px] rounded-tr-[12px] bg-[#ECF3E2E5] p-4">
+            {emotionKeys.map((key) => {
+              if (emotion === key) return null;
+              const isWhiteOption = key === "SAD" || key === "ANGRY";
+
+              return (
+                <button
+                  key={key}
+                  onClick={() => handleSelect(key)}
+                  className={`rounded-full border px-2 py-1 text-sm font-bold ${
+                    isWhiteOption ? "text-white" : "text-primary-700"
+                  }`}
+                  style={{
+                    backgroundColor: `var(--color-${EMOTION_COLOR[key]})`,
+                    borderColor: `var(--color-${EMOTION_BORDER_COLOR[key]})`,
+                  }}
+                >
+                  {EMOTIONS_QUERY[key]}
+                </button>
+              );
+            })}
+          </div>
+        )}
+
+        <p className="text-2xl font-bold text-white">듣기 좋은</p>
       </div>
-
-      {/* 드롭다운 목록 */}
-      {open && (
-        <div className="fixed grid max-w-72 grid-cols-3 gap-4 rounded-xl bg-primary-100 p-3">
-          {Object.entries(EMOTIONS_QUERY).map(([emotionKey, emotionLabel]) => {
-            if (emotion === emotionKey) return null;
-            const isSelected = emotion === emotionKey;
-
-            return (
-              <button
-                key={emotionKey}
-                onClick={() => handleSelect(emotionKey as keyof typeof Emotion)}
-                className={`w-full rounded-full border px-2 py-1 text-sm font-bold ${isSelected ? "text-primary-900 bg-primary-300" : "bg-white text-black hover:bg-gray-100"}`}
-                style={{
-                  backgroundColor: `var(--color-${EMOTION_COLOR[emotionKey]})`,
-                  borderColor: `var(--color-${EMOTION_BORDER_COLOR[emotionKey]})`,
-                }}
-              >
-                {emotionLabel}
-              </button>
-            );
-          })}
-        </div>
-      )}
-      <p className="text-xl font-bold text-white">
+      <p className="text-2xl font-bold text-white">
         플레이리스트를 추천해 드릴게요!
       </p>
-      <p className="mt-6 text-xs text-grey-2">
+      <p className="mt-6 text-grey-2">
         {!todayEmotion && (
           <span>
-            일기를 작성하고
+            오늘의 일기를 작성하면,
             <br />
-            오늘 감정에 맞는 플레이리스트를 추천받아보세요.
+            감정에 어울리는 음악을 추천해드려요.
           </span>
         )}
       </p>
