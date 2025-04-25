50541203d2d255f7c5b1fa6b983a17ad4e3e1069
Young <dndud5182@gmail.com>
2025-04-25 16:55:21 +0900

    style: 로그인 및 회원가입 페이지 반응형 수정

--- src/app/sign-up/_components/sign-up-form.tsx
+++ src/app/sign-up/_components/sign-up-form.tsx
@@ -33,13 +33,13 @@ const SignUpForm = () => {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-primary-50 px-10">
-      <div className="mb-10 flex flex-col items-center">
+    <div className="flex flex-col items-center justify-center gap-10 bg-primary-50 px-10">
+      <div className="flex flex-col items-center">
         <div className="mb-4 flex items-center justify-center rounded-full">
           <EmotionImage src={EMOTION_COOKIE_IMAGE_URL.EXCITED} size="l" />
         </div>
-        <strong className="mb-1 text-xl text-grey-6">Smookie</strong>
-        <p className="text-center text-grey-6">
+        <strong className="mb-1 text-xl text-grey-8">Smookie</strong>
+        <p className="text-center text-grey-5">
           하루의 끝, 마음을 조용히 들여다보고 싶을 때
         </p>
       </div>
@@ -83,15 +83,18 @@ const SignUpForm = () => {
         <Button
           type="submit"
           disabled={!isValid}
-          classname="w-full bg-primary-300 text-primary-800 py-3 rounded-xl mt-6"
+          classname="w-full bg-primary-300 text-primary-800 py-3 rounded-xl "
         >
           <strong>회원가입</strong>
         </Button>
       </form>
 
-      <div className="mt-10 w-full border border-grey-3" />
-      <div className="m-4 text-grey-3">
-        <Link href="/sign-in">이미 계정이 있으신가요?</Link>
+      <div className="flex w-full items-center gap-3 text-grey-3">
+        <div className="flex-grow border-b border-grey-3" />
+        <Link href="/sign-in" className="whitespace-nowrap">
+          이미 계정이 있으신가요?
+        </Link>
+        <div className="flex-grow border-b border-grey-3" />
       </div>
     </div>
   );
