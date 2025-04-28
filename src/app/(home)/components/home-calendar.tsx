0bca040ca3851c3de9d1b24e1e725e6ae7d83558
usuny0317 <usuny0317@naver.com>
2025-04-28 17:48:57 +0900

    style/ 홈 css 반응형 구현하기 #107

--- src/app/(home)/components/home-calendar.tsx
+++ src/app/(home)/components/home-calendar.tsx
@@ -73,7 +73,9 @@ const HomeCalendar = ({ userId }: { userId: string | undefined }) => {
     >
       <div className="flex items-center justify-between px-3 lg:px-5">
         <ChevronLeft onClick={handlePrevMonth} className="cursor-pointer" />
-        <HomeDate currentDate={currentDate} setCurrentDate={setCurrentDate} />
+        <div className="flex flex-1 justify-center overflow-hidden pb-1 md:justify-center">
+          <HomeDate currentDate={currentDate} setCurrentDate={setCurrentDate} />
+        </div>
         <ChevronRight
           onClick={() => {
             if (!isNextMonthFuture) handleNextMonth();
@@ -138,7 +140,7 @@ const HomeCalendar = ({ userId }: { userId: string | undefined }) => {
               key={idx}
               className={`relative flex h-[2.3rem] items-center justify-center rounded-md md:h-[5rem] lg:h-[5.8rem] ${
                 isFuture || day === null
-                  ? "text-grey-3"
+                  ? "text-grey-2"
                   : "cursor-pointer transition-all duration-200 ease-in-out hover:bg-secondary-50 md:hover:bg-[#F7FAF2]"
               }`}
             >
@@ -155,10 +157,10 @@ const HomeCalendar = ({ userId }: { userId: string | undefined }) => {
                     <span
                       className={`md:text-lg lg:text-[26px] lg:font-bold ${
                         idx % 7 === 6
-                          ? "text-primary-200"
+                          ? "text-primary-500"
                           : idx % 7 === 0
-                            ? "text-secondary-200"
-                            : "text-grey-4"
+                            ? "text-secondary-400"
+                            : ""
                       }`}
                     >
                       {day}
--- src/app/(home)/components/home-date.tsx
+++ src/app/(home)/components/home-date.tsx
@@ -1,76 +1,88 @@
 "use client";
 
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
-
 import dayjs from "dayjs";
 
 import { colors } from "@/constants/date-color";
 import { HomeDateProps } from "../type";
 
-// 공식에 공개되지 않은 MuiMonthCalendar, MuiYearCalendar을 사용하기 위해 타입을 지정합니다.
 declare module "@mui/material/styles" {
   interface ComponentNameToClassKey {
     MuiMonthCalendar: string;
     MuiYearCalendar: string;
   }
-
   interface Components {
     MuiMonthCalendar?: Components["MuiButton"];
     MuiYearCalendar?: Components["MuiButton"];
   }
 }
-const HomeDate = ({ currentDate, setCurrentDate }: HomeDateProps) => {
-  const customTheme = createTheme({
-    palette: {
-      primary: {
-        main: colors.secondary[200],
-        contrastText: colors.secondary[200],
-      },
-    },
-    components: {
-      //월별 설정
-      MuiMonthCalendar: {
-        styleOverrides: {
-          root: {
-            "& .MuiPickersMonth-monthButton.Mui-selected": {
-              backgroundColor: `${colors.secondary[50]} !important`,
-              color: colors.foreground,
-              "&:hover": {
-                backgroundColor: `${colors.secondary[300]} !important`,
-                color: colors.background,
-              },
-            },
-          },
-        },
-      },
-      //연도 설정
-      MuiYearCalendar: {
-        styleOverrides: {
-          root: {
-            "& .MuiPickersYear-yearButton.Mui-selected": {
-              backgroundColor: `${colors.secondary[50]} !important`,
-              color: colors.foreground,
-              "&:hover": {
-                backgroundColor: `${colors.secondary[300]} !important`,
-                color: colors.background,
-              },
-            },
-          },
-        },
-      },
-    },
-  });
 
+//직접 지정
+let customTheme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+    },
+  },
+  palette: {
+    primary: {
+      main: colors.secondary[200],
+      contrastText: colors.secondary[200],
+    },
+  },
+  components: {
+    MuiMonthCalendar: {
+      styleOverrides: {
+        root: {
+          "& .MuiPickersMonth-monthButton.Mui-selected": {
+            backgroundColor: `${colors.secondary[50]} !important`,
+            color: colors.foreground,
+            "&:hover": {
+              backgroundColor: `${colors.secondary[300]} !important`,
+              color: colors.background,
+            },
+          },
+        },
+      },
+    },
+    MuiYearCalendar: {
+      styleOverrides: {
+        root: {
+          "& .MuiPickersYear-yearButton.Mui-selected": {
+            backgroundColor: `${colors.secondary[50]} !important`,
+            color: colors.foreground,
+            "&:hover": {
+              backgroundColor: `${colors.secondary[300]} !important`,
+              color: colors.background,
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+customTheme = responsiveFontSizes(customTheme);
+
+const HomeDate = ({ currentDate, setCurrentDate }: HomeDateProps) => {
   return (
     <div
       onKeyDown={(e) => {
         e.preventDefault();
         e.stopPropagation();
       }}
-      className="item-center h-100% w-100% flex justify-center"
+      className="flex h-full w-full items-center justify-center"
     >
       <ThemeProvider theme={customTheme}>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -92,28 +104,31 @@ const HomeDate = ({ currentDate, setCurrentDate }: HomeDateProps) => {
                   readOnly: true,
                   onKeyDown: (e) => e.preventDefault(),
                 },
-                sx: {
+                sx: () => ({
                   width: "130px",
-                  height: "60px",
+                  height: "50px",
                   caretColor: "transparent",
                   "& .MuiOutlinedInput-notchedOutline": {
                     border: "none",
                   },
                   "& .MuiInputBase-root": {
                     backgroundColor: {
+                      xs: "white",
                       md: "#F7FAF2",
                     },
                     borderRadius: "12px",
                     width: {
+                      xs: "130px",
                       md: "150px",
                     },
                   },
                   "& .MuiInputBase-input": {
                     fontSize: {
+                      xs: "1rem",
                       md: "1.5rem",
                     },
                   },
-                },
+                }),
               },
             }}
           />
