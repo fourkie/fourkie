818f34b66eb594224995c30a2d8ffddee44860c8
Juha Yoon <ijooha16@gmail.com>
2025-04-28 17:44:30 +0900

    bugfix: build error

--- src/ui/common/cookie-alert.common.tsx
+++ src/ui/common/cookie-alert.common.tsx
@@ -2,10 +2,8 @@ import { AlertType } from "@/types/cookie-alert.type";
 
 const CookieAlert = ({ text, isContent = false }: AlertType) => {
   let common = "flex flex-col items-center justify-center gap-6 text-grey-6 ";
-  let size = "xs";
 
   if (!isContent) {
-    size = "l";
     common = common + "pt-24";
   }
 
