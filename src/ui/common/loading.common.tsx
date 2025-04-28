b30151bb3d2e27a66d6971b0a1795243d0e5cd34
Young <dndud5182@gmail.com>
2025-04-28 19:51:34 +0900

    add: LoadingCookie 컴포넌트 및 애니메이션 추가

---
+++ src/ui/common/loading.common.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { motion, Variants } from "framer-motion";
+
+const LoadingCookie = () => {
+  const dotVariants: Variants = {
+    jump: {
+      y: -30,
+      transition: {
+        duration: 0.5,
+        repeat: Infinity,
+        repeatType: "mirror",
+        ease: "easeInOut",
+      },
+    },
+  };
+
+  return (
+    <motion.div
+      animate="jump"
+      transition={{ staggerChildren: -0.2, staggerDirection: -1 }}
+      className="flex items-center justify-center gap-2"
+    >
+      <motion.div
+        className="h-10 w-10 rounded-full bg-[url('/images/joy.png')] bg-cover"
+        variants={dotVariants}
+      />
+      <motion.div
+        className="h-10 w-10 rounded-full bg-[url('/images/joy.png')] bg-cover"
+        variants={dotVariants}
+      />
+      <motion.div
+        className="h-10 w-10 rounded-full bg-[url('/images/joy.png')] bg-cover"
+        variants={dotVariants}
+      />
+    </motion.div>
+  );
+};
+
+export default LoadingCookie;
