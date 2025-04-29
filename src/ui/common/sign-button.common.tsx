73951d955bbb76109aa74cc06d92899a7c28043e
Juha Yoon <ijooha16@gmail.com>
2025-04-30 04:34:26 +0900

    style: 애니메이션

---
+++ src/ui/common/sign-button.common.tsx
@@ -0,0 +1,15 @@
+import { ButtonProps } from "@/types/components-props.type";
+
+const SignButton = ({ children, disabled, type }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      disabled={disabled}
+      className=" h-14 w-full cursor-pointer rounded-2xl bg-primary-300 text-lg text-primary-800 md:py-2"
+    >
+      <strong className="mx-auto">{children}</strong>
+    </button>
+  );
+};
+
+export default SignButton;
