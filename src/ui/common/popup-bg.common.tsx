af0a57726c2880a587ea7bdd5bcd0a32a2d5add8
Juha Yoon <ijooha16@gmail.com>
2025-04-29 23:18:11 +0900

    update: 팝업 배경 애니메이션

--- src/ui/common/popup-bg.common.tsx
+++ src/ui/common/popup-bg.common.tsx
@@ -1,10 +1,17 @@
+import { motion } from "framer-motion";
 import { ReactNode } from "react";
 
 const Popup = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="fixed inset-0 w-screen h-screen bg-black/70 z-50 flex justify-center items-center px-5">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.2 }}
+      className="fixed inset-0 z-50 flex h-screen w-screen items-center justify-center bg-black/70 px-5"
+    >
       {children}
-    </div>
+    </motion.div>
   );
 };
 
